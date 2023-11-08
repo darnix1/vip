c2812791f4e9ac777b5edb667e8db15e829741cd
darnix1 <146903037+darnix1@users.noreply.github.com>
2023-11-08 10:45:11 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -37,8 +37,9 @@ echo -e "$COLOR1 ESTDADO ACTUAL >> $stl  ${NC}"
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
 echo -e " $COLOR1 $NC ${WH}[${COLOR1}01${WH}]${NC} ${COLOR1}• ${WH} ACTUALIZAR $COLOR1 ${NC}"
 echo -e " $COLOR1 $NC                                              ${NC} $COLOR1 $NC"                                            ${NC} $COLOR1 $NC"
-echo -e " $COLOR1 $NC ${WH}[${COLOR1}02${WH}]${NC} ${COLOR1}• ${WH}MENU $COLOR1 $NC"
-echo -e " $COLOR1 $NC                                              ${NC} $COLOR1 $NC" 
+
+echo -e " $COLOR1 $NC ${WH}[${COLOR1}01${WH}]${NC} ${COLOR1}• ${WH} MENU $COLOR1 ${NC}"
+echo -e " $COLOR1 $NC                                              ${NC} $COLOR1 $NC"                                            ${NC} $COLOR1 $NC"                                              ${NC} $COLOR1 $NC" 
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo -e ""
 read -p "  Escoge 1, x , y : " option2
