525dc41bead3c2465c7e7c9cfbbd9d757ac81b0d
darnix1 <146903037+darnix1@users.noreply.github.com>
2023-11-14 22:12:07 -0600

    Update update.sh

--- backup/update.sh
+++ backup/update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/Hermananza/vip/main/menu/menu.zip
+    wget https://raw.githubusercontent.com/darnix1/vip/main/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
