3a3fa06793bc6c5bad01ca80fa9a798cf4a7ae28
darnix1 <146903037+darnix1@users.noreply.github.com>
2024-05-14 16:42:37 -0600

    Update x.sh

--- backup/x.sh
+++ backup/x.sh
@@ -245,8 +245,8 @@ rm -f /bin/cgh > /dev/null
 rm -rf /bin/ejecutar > /dev/null
 #figlet " Key Invalida" | boxes -d stone -p a2v1 > error.log
 msg -bar3 >> error.log
-echo "  Key Invalida, Contacta con tu Provehedor" >> error.log
-echo -e ' https://t.me/ChumoGH  - @drowkid01' >> error.log
+echo "  Contacta con tu Provehedor" >> error.log
+echo -e ' Para verificar el error ' >> error.log
 msg -bar3 >> error.log
 #cat error.log | lolcat
 #msg -bar3
@@ -280,7 +280,7 @@ msg -bar3
 figlet ' -DARNIX- ' | boxes -d stone -p a0v0 | lolcat
 echo "           PEGA TU KEY DE INSTALACION " 
 echo -ne " " && msg -bar3
-echo -ne " \033[1;41m Key : \033[0;33m" && read Key
+echo -ne " \033[1;41m Clave : \033[0;33m" && read Key
 tput cuu1 && tput dl1
 done
 Key="$(echo "$Key" | tr -d '[[:space:]]')"
@@ -451,7 +451,8 @@ update_pak () {
 echo "" 
 [[ $(dpkg --get-selections|grep -w "pv"|head -1) ]] || apt install pv -y &> /dev/null 
 os_system 
-echo -e "		  ESPERE UN MOMENTO  "
+e#cho -e "		  ESPERE UN MOMENTO  "
+echo -e "\033[1;37m     AGUARDE UN MOMENTO \033[1;32m.\033[1;32m.\033[1;33m.\033[1;31m. \033[1;33m"
 [[ $(dpkg --get-selections|grep -w "lolcat"|head -1) ]] || apt-get install lolcat -y &>/dev/null 
 [[ $(dpkg --get-selections|grep -w "figlet"|head -1) ]] || apt-get install figlet -y &>/dev/null
 [[ $(dpkg --get-selections|grep -w "figlet"|head -1) ]] || apt-get install  -y &>/dev/null
@@ -469,20 +470,20 @@ echo ""
 msg -bar3
 exit && exit
 }
-echo -e "\e[1;31m  SISTEMA:  \e[33m$distro $vercion \e[1;31m	CPU:  \e[33m$(lscpu | grep "Vendor ID" | awk '{print $3}')" 
+#echo -e "\e[1;31m  SISTEMA:  \e[33m$distro $vercion \e[1;31m	CPU:  \e[33m$(lscpu | grep "Vendor ID" | awk '{print $3}')" 
 msg -bar3
-dpkg --configure -a > /dev/null 2>&1 && echo -e "\033[94m    ${TTcent} INTENTANDO RECONFIGURAR UPDATER ${TTcent}" | pv -qL 80
-msg -bar3
-echo -e "\033[94m    ${TTcent} UPDATE DATE : $(date +"%d/%m/%Y") & TIME : $(date +"%H:%M") ${TTcent}" | pv -qL 80
+#dpkg --configure -a > /dev/null 2>&1 && echo -e "\033[94m    ${TTcent} INTENTANDO RECONFIGURAR UPDATER ${TTcent}" | pv -qL 80
+#msg -bar3
+#echo -e "\033[94m    ${TTcent} UPDATE DATE : $(date +"%d/%m/%Y") & TIME : $(date +"%H:%M") ${TTcent}" | pv -qL 80
 [[ $(dpkg --get-selections|grep -w "net-tools"|head -1) ]] || apt-get install net-tools -y &>/dev/null
 [[ $(dpkg --get-selections|grep -w "boxes"|head -1) ]] || apt-get install boxes -y &>/dev/null
-msg -bar3
-apt-get install software-properties-common -y > /dev/null 2>&1 && echo -e "\033[94m    ${TTcent} INSTALANDO NUEVO PAQUETES ( S|P|C )    ${TTcent}" | pv -qL 80
+#msg -bar3
+apt-get install software-properties-common -y > /dev/null 2>&1 && echo -e "\033[94m    INSTALANDO NUEVO PAQUETES    " | pv -qL 80
 msg -bar3
 #echo -e "\033[94m    ${TTcent} PREPARANDO BASE RAPIDA INSTALL    ${TTcent}" | pv -qL 80 
-msg -bar3
+#msg -bar3
 #echo -e "\033[94m    ${TTcent} CHECK IP FIJA $(wget -qO- ifconfig.me)    ${TTcent}" | pv -qL 80 
-msg -bar3
+#msg -bar3
 echo " "
 sleep 2s
 #[[ $(dpkg --get-selections|grep -w "figlet"|head -1) ]] || apt-get install figlet -y -qq --silent &>/dev/null
