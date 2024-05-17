89b222d00570730be46327defcd1e8d7a9d14b4f
darnix1 <146903037+darnix1@users.noreply.github.com>
2024-05-17 11:53:06 -0600

    Update x.sh

--- backup/x.sh
+++ backup/x.sh
@@ -3,19 +3,24 @@ helicex() {
     tput civis
     colors=("31" "32" "33" "34" "35")  # Lista de códigos de color ANSI para los puntos
     end_time=$((SECONDS + 5))  # Define el tiempo de finalización a 5 segundos en el futuro
+    final_line=""
 
     while [ $SECONDS -lt $end_time ]; do
         for i in {1..10}; do
             color_index=$(( (i-1) % ${#colors[@]} ))  # Calcula el índice del color
             color="\033[${colors[$color_index]}m"  # Obtiene el código de color ANSI
-            echo -ne "\r${color}$(printf '●%.0s' $(seq $i))\033[0m     "
+            line="${color}$(printf '●%.0s' $(seq $i))\033[0m     "
+            echo -ne "\r${line}"
             sleep 0.1
             if [ $SECONDS -ge $end_time ]; then
+                final_line=$line
                 break  # Sale del bucle for si el tiempo ha alcanzado o excedido 5 segundos
             fi
         done
     done
 
+    # Imprimir la línea final y restaurar el cursor
+    echo -e "\r${final_line}"
     tput cnorm
 }
 
