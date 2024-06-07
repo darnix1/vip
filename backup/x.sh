244d7a41f2130e559928265a6eea17075c3ba5a7
darnix1 <146903037+darnix1@users.noreply.github.com>
2024-06-07 12:38:50 -0600

    Update x.sh

--- backup/x.sh
+++ backup/x.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ -e /bin/xdarnix/msg ]] && source /bin/xdarnix/msg || source <(curl -sSL https://raw.githubusercontent.com/emirjorge/Script-Z/master/CHUMO/msg-bar/msg)
+[[ -e /bin/xdarnix/msg ]] && source /bin/xdarnix/msg || source <(curl -sSL https://gitea.com/xdarnix/msg/raw/branch/main/msg)
 
 
 print_center "Hola "
